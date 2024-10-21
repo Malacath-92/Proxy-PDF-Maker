1e0de1ed8ecb211a2f1869b11c0fcbb33ff23f34
Mal <vingine.dev@gmail.com>
2024-10-21 12:51:50 +0200

    Initial commit

---
+++ source/version/version.cpp
@@ -0,0 +1,13 @@
+#include "version.h"
+
+#define STRINGIFY(x) #x
+#define TOSTRING(x) STRINGIFY(x)
+
+std::string_view proxy_pdf_version()
+{
+#ifdef PROXY_PDF_VERSION
+    return TOSTRING(PROXY_PDF_VERSION);
+#else
+    return "<unkown version>";
+#endif
+}
