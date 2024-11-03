bb8d9cab09437035df89fd091bfbda9571733b50
Mal <vingine.dev@gmail.com>
2024-11-03 19:16:02 +0100

    Add build time for fun

--- source/version/ppp/version.cpp
+++ source/version/ppp/version.cpp
@@ -11,3 +11,12 @@ std::string_view proxy_pdf_version()
     return "<unkown version>";
 #endif
 }
+
+std::string_view proxy_pdf_build_time()
+{
+#ifdef PROXY_PDF_NOW
+    return TOSTRING(PROXY_PDF_NOW);
+#else
+    return "<unkown build time>";
+#endif
+}
--- source/version/ppp/version.hpp
+++ source/version/ppp/version.hpp
@@ -3,3 +3,4 @@
 #include <string_view>
 
 std::string_view proxy_pdf_version();
+std::string_view proxy_pdf_build_time();
