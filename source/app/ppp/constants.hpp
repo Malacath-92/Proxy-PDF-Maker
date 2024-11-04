53633cf1b2411c3e785da2055ca45961907d73c9
Mal <vingine.dev@gmail.com>
2024-11-04 15:35:44 +0100

    Add supported page sizes

--- source/app/ppp/constants.hpp
+++ source/app/ppp/constants.hpp
@@ -1,13 +1,22 @@
 #pragma once
 
 #include <string_view>
+#include <unordered_map>
+
+#include <hpdf_types.h>
 
 #include <ppp/util.hpp>
 
 std::string_view cwd();
 
-//page_sizes = {"Letter": LETTER, "A5": A5, "A4": A4, "A3": A3, "Legal": LEGAL}
+inline const std::unordered_map<std::string_view, HPDF_PageSizes> PageSizes{
+    { "Letter", HPDF_PAGE_SIZE_LETTER },
+    { "A5", HPDF_PAGE_SIZE_A5 },
+    { "A4", HPDF_PAGE_SIZE_A4 },
+    { "A3", HPDF_PAGE_SIZE_A3 },
+    { "Legal", HPDF_PAGE_SIZE_LEGAL },
+};
 
-inline constexpr Size card_size_with_bleed{ 2.72_in, 3.7_in };
-inline constexpr Size card_size_without_bleed{ 2.48_in, 3.46_in };
-inline constexpr float card_ratio{ card_size_without_bleed.x / card_size_without_bleed.y };
+inline constexpr Size CardSizeWithBleed{ 2.72_in, 3.7_in };
+inline constexpr Size CardSizeWithoutBleed{ 2.48_in, 3.46_in };
+inline constexpr float CardRatio{ CardSizeWithoutBleed.x / CardSizeWithoutBleed.y };
