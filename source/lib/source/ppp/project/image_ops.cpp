ec3573b04506aa819164410c7a1bf2f074d5eb67
Mal <vingine.dev@gmail.com>
2025-03-28 12:34:17 +0100

    Fix printout

--- source/lib/source/ppp/project/image_ops.cpp
+++ source/lib/source/ppp/project/image_ops.cpp
@@ -60,7 +60,7 @@ Image CropImage(const Image& image, const fs::path& image_name, Length bleed_edg
     {
         const PixelSize new_size{ dla::round(cropped_image.Size() * (max_density / density)) };
         const PixelDensity max_dpi{ (max_density * 1_in / 1_m) };
-        PPP_LOG("Cropping images...\n{} - Exceeds maximum DPI {}, resizing to {}", max_dpi.value, image_name.string(), new_size);
+        PPP_LOG("Cropping images...\n{} - Exceeds maximum DPI {}, resizing to {}", image_name.string(), max_dpi.value, static_cast<dla::uvec2>(new_size / 1_pix));
         return cropped_image.Resize(new_size);
     }
     return cropped_image;
