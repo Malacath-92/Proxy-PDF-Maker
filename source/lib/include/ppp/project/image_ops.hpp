a61245f08c3f2feb608342656b34ff6b8dcc71c7
Mal <vingine.dev@gmail.com>
2025-04-01 08:42:28 +0200

    Get rid of old full cropper

--- source/lib/include/ppp/project/image_ops.hpp
+++ source/lib/include/ppp/project/image_ops.hpp
@@ -34,18 +34,6 @@ Image UncropImage(const Image& image, const fs::path& image_name, PrintFn print_
 
 fs::path GetOutputDir(const fs::path& crop_dir, Length bleed_edge, const std::string& color_cube_name);
 
-bool NeedRunCropper(const fs::path& image_dir, const fs::path& crop_dir, Length bleed_edge, const std::string& color_cube_name);
-ImgDict RunCropper(const fs::path& image_dir,
-                   const fs::path& crop_dir,
-                   const fs::path& img_cache_file,
-                   const ImgDict& img_dict,
-                   Length bleed_edge,
-                   PixelDensity max_density,
-                   const std::string& color_cube_name,
-                   const cv::Mat* color_cube,
-                   bool uncrop,
-                   PrintFn print_fn);
-
 bool NeedRunMinimalCropper(const fs::path& image_dir,
                            const fs::path& crop_dir,
                            std::span<const fs::path> card_list,
