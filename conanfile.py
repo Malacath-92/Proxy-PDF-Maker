991173f940b5a072bb531023cd5db053aa5adf19
Mal <vingine.dev@gmail.com>
2025-04-14 15:44:35 +0200

    Revert "Depend on harfbuzz without glib"
    This reverts commit 1950a3484699e729cd30f1dbc7dc44ea5baa7690.

--- conanfile.py
+++ conanfile.py
@@ -51,8 +51,6 @@ class ProxyPDF(ConanFile):
         self.options["opencv"].with_imgcodec_pxm = False
         self.options["opencv"].with_imgcodec_sunraster = False
 
-        self.options["harfbuzz"].with_glib = False
-
         self.options["pdf-writer"].shared = False
         self.options["pdf-writer"].with_tiff = False
 
