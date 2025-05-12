055184125231f9534c67d6c1ac2ef0b11a18ba62
Hanno <vingine.dev@gmail.com>
2025-05-12 13:40:28 +0200

    Remove unused old cropper code

--- source/lib/include/ppp/project/image_ops.hpp
+++ source/lib/include/ppp/project/image_ops.hpp
@@ -37,21 +37,6 @@ Image UncropImage(const Image& image, const fs::path& image_name, Size card_size
 
 fs::path GetOutputDir(const fs::path& crop_dir, Length bleed_edge, const std::string& color_cube_name);
 
-bool NeedRunMinimalCropper(const fs::path& image_dir,
-                           const fs::path& crop_dir,
-                           std::span<const fs::path> card_list,
-                           Length bleed_edge,
-                           const std::string& color_cube_name);
-void RunMinimalCropper(const fs::path& image_dir,
-                       const fs::path& crop_dir,
-                       std::span<const fs::path> card_list,
-                       Size card_size,
-                       Length full_bleed,
-                       Length bleed_edge,
-                       PixelDensity max_density,
-                       const std::string& color_cube_name,
-                       const cv::Mat* color_cube);
-
 ImgDict ReadPreviews(const fs::path& img_cache_file);
 void WritePreviews(const fs::path& img_cache_file, const ImgDict& img_dict);
 
