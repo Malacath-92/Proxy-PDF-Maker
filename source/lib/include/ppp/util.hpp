dd48a3856a0c81cd325c8a53abc24ef44207d728
Hanno <vingine.dev@gmail.com>
2026-02-14 22:10:26 +0100

    Make sure AtScopeExit isn't movable and copyable

--- source/lib/include/ppp/util.hpp
+++ source/lib/include/ppp/util.hpp
@@ -129,10 +129,21 @@ bool OpenPath(const fs::path& path);
 template<class FunT>
 struct AtScopeExit
 {
+    AtScopeExit() = delete;
+    AtScopeExit(const AtScopeExit&) = delete;
+    AtScopeExit(AtScopeExit&&) = delete;
+    AtScopeExit& operator=(const AtScopeExit&) = delete;
+    AtScopeExit& operator=(AtScopeExit&&) = delete;
+
+    AtScopeExit(FunT fun)
+        : m_Dtor{ std::move(fun) }
+    {}
+
     ~AtScopeExit()
     {
         m_Dtor();
     }
+
     FunT m_Dtor;
 };
 
