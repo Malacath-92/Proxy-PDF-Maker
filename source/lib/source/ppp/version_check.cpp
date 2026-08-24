72b04b2783aeffe791b0f54d4e027f39ce250c59
Mal <vingine.dev@gmail.com>
2026-08-24 22:13:29 +0200

    Only download Nightly from Nightly and also only download it if it is newer

--- source/lib/source/ppp/version_check.cpp
+++ source/lib/source/ppp/version_check.cpp
@@ -4,8 +4,11 @@
 
 #include <fmt/format.h>
 
+#include <QDateTime>
 #include <QEventLoop>
+#include <QJsonArray>
 #include <QJsonDocument>
+#include <QJsonObject>
 #include <QJsonValue>
 #include <QMetaEnum>
 #include <QNetworkAccessManager>
@@ -18,15 +21,70 @@
 #include <ppp/util/log.hpp>
 #include <ppp/version.hpp>
 
-inline constexpr std::string_view c_LatestReleaesURI{
+inline constexpr std::string_view c_AllReleasesURI{
+    "https://api.github.com/repos/Malacath-92/Proxy-PDF-Maker/releases"
+};
+inline constexpr std::string_view c_LatestReleaseURI{
     "https://api.github.com/repos/Malacath-92/Proxy-PDF-Maker/releases/latest"
 };
 
-std::optional<std::string> NewAvailableVersion()
+std::optional<QJsonObject> GetLatestPreRelease()
 {
     QNetworkAccessManager network_manager;
 
-    QNetworkRequest request{ PrepareGithubRequest(ToQString(c_LatestReleaesURI)) };
+    QNetworkRequest request{ PrepareGithubRequest(ToQString(c_AllReleasesURI)) };
+    QNetworkReply* reply{ network_manager.get(std::move(request)) };
+
+    {
+        QEventLoop loop;
+        QObject::connect(reply, &QNetworkReply::finished, &loop, &QEventLoop::quit);
+        loop.exec();
+    }
+
+    AtScopeExit delete_reply{
+        [reply]()
+        { delete reply; }
+    };
+
+    if (reply->error() != QNetworkReply::NetworkError::NoError)
+    {
+        LogWarning("Failed fetching all releases: {}",
+                   QMetaEnum::fromType<QNetworkReply::NetworkError>().valueToKey(reply->error()));
+        return std::nullopt;
+    }
+
+    const auto reply_json{ QJsonDocument::fromJson(reply->readAll()) };
+    if (reply_json.isEmpty())
+    {
+        LogWarning("Empty reply for all releases.");
+        return std::nullopt;
+    }
+
+    if (!reply_json.isArray())
+    {
+        LogWarning("All releases did not return an array.");
+        return std::nullopt;
+    }
+
+    QJsonArray releases = reply_json.array();
+    for (const QJsonValue& value : releases)
+    {
+        QJsonObject release = value.toObject();
+        if (release.value("prerelease").toBool())
+        {
+            return release;
+        }
+    }
+
+    LogWarning("Did not find any pre-release.");
+    return std::nullopt;
+}
+
+std::optional<QJsonDocument> GetLatestRelease()
+{
+    QNetworkAccessManager network_manager;
+
+    QNetworkRequest request{ PrepareGithubRequest(ToQString(c_LatestReleaseURI)) };
     QNetworkReply* reply{ network_manager.get(std::move(request)) };
 
     {
@@ -54,21 +112,65 @@ std::optional<std::string> NewAvailableVersion()
         return std::nullopt;
     }
 
-    const auto latest_version{ reply_json["tag_name"].toString().toStdString() };
+    return reply_json;
+}
+
+std::optional<std::string> NewAvailableVersion()
+{
     const auto this_version{ ProxyPdfVersion() };
-
-    const auto latest_semver{ ProxyPdfToSemanticVersion(latest_version) };
     const auto this_semver{ ProxyPdfToSemanticVersion(this_version) };
+    const auto is_nightly{ this_semver == SemanticVersion{ 0, 0, 0 } };
 
-    if (latest_semver <= this_semver)
+    if (is_nightly)
     {
-        LogInfo("Latestst release is not newer than current: {} <= {}",
-                latest_version,
-                this_version);
+        LogInfo("This release is a Nightly release, looking for newer Nightly.");
+        if (const auto latest_pre_release{ GetLatestPreRelease() })
+        {
+            const auto this_created_at{ ToQString(ProxyPdfBuildTime()) };
+            const auto latest_created_at{ latest_pre_release.value().value("created_at").toString() };
+
+            const auto this_time{ QDateTime::fromString(this_created_at, Qt::ISODate) };
+            const auto latest_time{ QDateTime::fromString(latest_created_at, Qt::ISODate) };
+
+            if (!this_time.isValid())
+            {
+                LogWarning("Could not parse local build time...");
+                return std::nullopt;
+            }
+            if (!latest_time.isValid())
+            {
+                LogWarning("Could not pre-release build time...");
+                return std::nullopt;
+            }
+
+            if (latest_time.toMSecsSinceEpoch() <= this_time.toMSecsSinceEpoch())
+            {
+                LogWarning("Latest pre-release is not newer that this release...");
+                return std::nullopt;
+            }
+
+            return latest_pre_release.value().value("tag_name").toString().toStdString();
+        }
         return std::nullopt;
     }
 
-    return latest_version;
+    if (const auto latest_release{ GetLatestRelease() })
+    {
+        const auto latest_version{ latest_release.value()["tag_name"].toString().toStdString() };
+        const auto latest_semver{ ProxyPdfToSemanticVersion(latest_version) };
+
+        if (latest_semver <= this_semver)
+        {
+            LogInfo("Latest release is not newer than current: {} <= {}",
+                    latest_version,
+                    this_version);
+            return std::nullopt;
+        }
+
+        return latest_version;
+    }
+
+    return std::nullopt;
 }
 
 std::string ReleaseURL(std::string_view version)
